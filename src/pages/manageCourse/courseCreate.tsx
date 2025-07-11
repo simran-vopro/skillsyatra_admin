fd05934b294911852fbeff9a59ab18ed4e241533
Simran <sktech@vopro.in>
2025-07-11 18:34:13 +0530

    done

--- src/pages/manageCourse/courseCreate.tsx
+++ src/pages/manageCourse/courseCreate.tsx
@@ -4,8 +4,6 @@ import Input from "../../components/form/input/InputField";
 import TextArea from "../../components/form/input/TextArea";
 import Select from "../../components/form/input/SelectField";
 import Button from "../../components/ui/button/Button";
-
-
 import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css';
 
